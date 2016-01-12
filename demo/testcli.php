dcbc1b2efc75c5a361fa098f398db0014befe336
A1essandro <alexander.yermakov.1990@gmail.com>
2016-01-12 12:10:28 +0400

    Added report of memory usage to CLI demo

--- demo/testcli.php
+++ demo/testcli.php
@@ -13,10 +13,15 @@ $gen->setSize($size);
 $gen->setPersistence(50);
 $gen->setMapSeed('uniqueSeed');
 
+$memStart = memory_get_usage();
 if ($size > 3) {
     $gen->generate();
 } else {
     print_r($gen->generate());
 }
 
-echo sprintf('Time: %s', round(microtime(true) - $start, 3));
+echo sprintf('Memory Limit: %s', ini_get('memory_limit')) . PHP_EOL;
+echo sprintf('Memory Usage: %s', round((memory_get_usage() - $memStart) / 1024 / 1024, 2)) . 'M' . PHP_EOL;
+echo sprintf('Memory Peak Usage: %s', round(memory_get_peak_usage() / 1024 / 1024, 2)) . 'M' . PHP_EOL;
+
+echo sprintf('Time: %s', round(microtime(true) - $start, 3)) . PHP_EOL;
